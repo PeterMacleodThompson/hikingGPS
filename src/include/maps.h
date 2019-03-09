45811ab7b237a628c5b59cf1d82a3d36392862e1
Peter Macleod Thompson <peter.macleod.thompson@gmail.com>
2019-03-08 20:05:01 -0700

    kernel-doc comments for maps.h
    maps.h comments updated using kernel-doc format.
    Describes structure of maps.
    
    Signed-off-by: Peter Macleod Thompson <peter.macleod.thompson@gmail.com>

--- src/include/maps.h
+++ src/include/maps.h
@@ -21,7 +21,7 @@
  *      Ctopo MAP HEIGHT = 15 minutes 	3600 pixels fixed
  *      Ctopo SIZE 	630 gigbytes; 13909 maps (du -h;ls -l | wc -l)
  *	Ctopo map of Canada is 5.3km x 4.2km
-
+ *
  *      C250k MAP WIDTH = 2 degrees	8707 pixels varies  map to map
  *      C250k MAP HEIGHT = 1 degree 	6758 pixels varies somewhat map to map
  *      C250K SIE 	80 gigbytes; 915 maps (du -h; ls -l | wc -l)
@@ -32,7 +32,7 @@
  *      CIMW SIZE 	1.7 gigbytes; 78 maps (du -h; ls -l | wc -l)
  *      CIMW map of Canada is 27m x 25m
  *
-*/
+ */
 
 /**
  * DOC: Example mapset structure for Canada
@@ -46,74 +46,84 @@
  *      		loaded from /MAPSPATH/mapset
  *      PMTmapset
  *        -----------------------------------------
- *        |C50k		| pointer to C50k mapindex	
- *        |-----------------------------------------	
+ *        |C50k		| pointer to C50k mapindex
+ *        |-----------------------------------------
  *        |C250k	| pointer to C250k mapindex
  *        |-----------------------------------------
- *        |CIMW		| pointer to CIMW mapindex 
+ *        |CIMW		| pointer to CIMW mapindex
  *        |-----------------------------------------
  *
  *
  *	PMTmapindex
- *      loaded from /MAPSPATH/C50k/mapindex		
- *        |-----------------------------------------	
- *	  |dddmmddmm key	| map filename to load		
+ *      loaded from /MAPSPATH/C50k/mapindex
+ *        |-----------------------------------------
+ *	  |dddmmddmm key	| map filename to load
  *        |----------------------------------------
- *		 ... approx 13,909 records ...	
+ *		 ... approx 13,909 records ...
  *
- *		
+ *
  *	PMTmapindex
  *	loaded from /MAPSPATH/C250k/mapindex
- *        |-----------------------------------------	
- *	  |dddmmddmm key	| map filename to load	
- *        |---------------------------------------- 
- *		... approx 915 records ...		
+ *        |-----------------------------------------
+ *	  |dddmmddmm key	| map filename to load
+ *        |----------------------------------------
+ *		... approx 915 records ...
  *
  *
  *      PMTmapindex
- *	loaded from /MAPSPATH/CIMW/mapindex 
- *       |-----------------------------------------  
- *	 |dddmmddmm key	| mapfilename to load	
+ *	loaded from /MAPSPATH/CIMW/mapindex
+ *       |-----------------------------------------
+ *	 |dddmmddmm key	| mapfilename to load
  *       |----------------------------------------
- *       |    ... approx 78 records ...		
+ *            ... approx 78 records ...
  *
  *
  *
  */
 
 /**
- * typedef struct PMTmapindex
+ * typedef struct PMTmapindex - list of all maps (filenames) in 1 mapset.
+ * @mapkey: format is dddmmddmm = longitude, latitude of NW corner of map.
+ * @mapwidth: format is ddmm = degrees*100+minutes
+ * @mapheight: format is ddmm = degrees*100+minutes
+ * @mapname: map filename. Normally map is a .png image
  *
- * mapindex file contains list of all maps (filenames) in 1 mapset.
- * One mapset per mapfolder.
- * map files are located in folder in mapfolder name defined in PMTmapset.
- * mapkey is dddmmddmm = longitude, latitude of NW corner of map.
- * sorted by mapkey to enable binary search via linux:
- * sort -k1n unsortedmapindex > mapindex
+ * PMTmapindex structure is loaded from the mapindex file.
+ * There is one mapindex file per mapfolder sorted by mapkey to enable binary
+ * search mapindex file is sorted via linux command sort -k1n unsortedmapindex >
+ * mapindex prior to loading into PMTmapindex structure The mapfolder directory
+ * contains the mapindex file (called mapindex) plus all maps (identified by map
+ * filename). Each mapfolder directory located at datapath/maps/PMTmapset.name
  */
 typedef struct PMTmapindex { /* key = top left (NorthWest) corner of map  */
   int mapkey;                /* format dddmmddmm = longitude*10000+latitude */
-  int mapwidth;              /* format is ddmm = degrees*100+minutes */
-  int mapheight;             /* format is ddmm = degrees*100+minutes */
-  char mapname[50];          /* map filename */
+  int mapwidth;
+  int mapheight;
+  char mapname[50];
 } PMTmapindex;
 
-
-
 /**
- * typedef struct PMTmapset
+ * typedef struct PMTmapset - list of all mapsets known to hikingGPS
+ * @sequence: NOT loaded from  mapset file - generated with bookends 0, 9999
+ * @name: the directory containing mapindex file and all map files for this
+ * mapset
+ * @usage: max 15 char string; eg golf,marine, aeronautic, topograpic, satellite
+ * @Kscale: 50 means 1:50,000 scale.  Used for zooming, panning properly
+ * @mapcount: number of maps in mapset for *mapindexPtr
+ * @mapindexPtr NOT loaded from mapset file - pointer to mapcount records
  *
- *list of mapsets sorted by increasing kscale = order to be viewed 
+ * list of mapsets sorted by increasing kscale = order to be viewed.
+ * mapset name is max 10 char string.  Is also the directory name of
+ * the mapfolder containing the mapindex file and all map (image) files.
+ * example mapset names for Canada are CIMW C250k, C50k, Ctopo, Cphoto
  */
 typedef struct PMTmapset {
-  int sequence;             /* NOT in file - generated with bookends 0, 9999 */
-  char name[11];            /* {CIMW C250k, C50k, Ctopo, Cphoto} */
-                            /* max 10 char + string terminator = 11 */
-  char usage[16];           /* golf,marine, aeronautic, topograpic, satellite */
-                            /* max 15 char + string terminator = 16 */
-  int Kscale;               /* scale of mapset - 50 means 1:50,000 scale */
-  int mapcount;             /* number of maps in mapset for *mapindexPtr */
-  PMTmapindex *mapindexPtr; /* NOT in file - pointer to mapcount records */
+  int sequence;
+  char name[11];
+  char usage[16];
+  int Kscale;
+  int mapcount;
+  PMTmapindex *mapindexPtr;
 } PMTmapset;
 
 #define MAXMAPSETS 10
@@ -127,5 +137,3 @@ typedef struct PMTmapset {
 #define movemapSF 0x0020
 
 extern char *datapath;
-
-
